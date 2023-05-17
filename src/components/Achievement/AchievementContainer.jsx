89abeacdaefccd6a876532c94dafd2151d15e2bc
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-05-18 00:34:08 +0600

    Design Update

--- src/components/Achievement/AchievementContainer.jsx
+++ src/components/Achievement/AchievementContainer.jsx
@@ -2,10 +2,10 @@ import {useEffect, useState} from "react";
 import Achievement from "./Achievement";
 import * as React from "react";
 
-export default function AchievementContainer () {
+export default function AchievementContainer() {
   const [showAchievement, setShowAchievement] = useState(false)
   useEffect(() => {
-    const timer = setTimeout(() => setShowAchievement(true), 600);
+    const timer = setTimeout(() => setShowAchievement(true), 1000);
     return () => clearTimeout(timer);
   }, []);
 
